b8cf642f043c11d23208971c68e850e5c55ffd61
Him007n <nagarhappy326@gmail.com>
2022-07-31 00:27:46 +0530

    2nd commit

---
+++ .vscode/13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,i,j;
+    cin>> a;
+
+    for ( i = 1; i <= a; i++)
+    {
+        for ( j = 1; j <= i; j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    
+}
